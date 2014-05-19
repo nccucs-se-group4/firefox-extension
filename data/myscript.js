874f0fe47eadb2a2109a771d4999ba054f17f652
CatkinGG <patrick.catkingg@gmail.com>
2014-05-19 22:29:46 +0800

    modify

--- data/myscript.js
+++ data/myscript.js
@@ -1,7 +1,8 @@
-window.onload = function(){
+self.port.on('findLyInform', function(tag){
 
-    var objects = document.getElementById("mediaarticlebody").getElementsByTagName("p");
+    var objects = document.getElementById("mediaarticlebody").getElementsByTagName(tag);
     var content;
+    var lyInform="";
 
     for(var i=0;i<objects.length;i++){
         content += objects[i].innerHTML;
@@ -10,8 +11,8 @@ window.onload = function(){
     for(var i=0;i<LyList.length;i++){
         var reg = new RegExp(LyList[i].name);
         if(reg.test(content)){
-            alert(LyList[i].name+" "+LyList[i].party);    
+ 	    lyInform += (LyList[i].name+"\n");
         }
     }
-    
-}
+    return self.port.emit('lyInform',lyInform);
+})
